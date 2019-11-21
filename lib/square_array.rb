21317572be5b22804c97e96638a14bba4bbc06ab
Anna Dybas <a.m.dybas@gmail.com>
2019-11-21 04:50:36 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
    counter = 0
    squared_numbers = []
    while array[counter] do
-     squared_numbers << array[counter] ** array[counter]
+     squared_numbers << array[counter] ** 2
      counter += 1
    end
    squared_numbers
